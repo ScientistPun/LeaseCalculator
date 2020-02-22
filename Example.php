5564c725bef2a07a7e9fe903b17ba0e739b319fb
scientistpun <honynero@sina.cn>
2020-02-23 02:48:14 +0800

    修改不能选择提前日期BUG

--- Example.php
+++ Example.php
@@ -6,35 +6,41 @@ if (isset($_GET['compute'])) {
     $rent = $_GET['rent'];
     $mode = $_GET['mode'];
     $forward = $_GET['forward'];
+    $forwardday = $_GET['forwardday'];
     $rentday = $_GET['rentday'];
 
     list($start, $end) = explode(' ~ ', $renttime);
 
-    $comupte = new \Lease\Compute(['mode' => $mode]);
+    $comupte = new \Lease\Compute(['mode' => $mode, 'forward'=> $forward]);
     $options = [
         'starttime'     => strtotime($start),   // 开始时间 
         'endtime'       => strtotime($end),     // 结束时间
         'rent'          => $rent,               // 租金
+        'rentday'       => $rentday,               // 固定收租日
     ];
 
-    if ($mode == 1 && $forward = 1) {
+    if ($mode == 1 && $forward) {
         // 提前收租日
         $options['forwardday'] = $forwardday;
-    } else {
-        // 固定收租日
-        $options['rentday'] = $rentday;
     }
 
     $comupte->setOptions($options);
     $data = $comupte->getComputeCycle();
+    if (!is_array($data)) {
+        echo json_encode(['code' => 2, 'msg' => $comupte->getError()]);
+        exit;
+    }
+
     foreach ($data as &$d) {
+        $d['pay'] = date('Y-m-d', $d['pay']);
         $d['start'] = date('Y-m-d', $d['start']);
         $d['end'] = date('Y-m-d', $d['end']);
         $d['rent'] = bcadd($d['rent'], 0, 2);
     }
     unset($d);
 
-    echo json_encode(['code' => 0, 'msg' => 'ok', 'count' => count($data), 'data' => $data]);
+
+    echo json_encode(['code' => 0, 'msg' => 'ok', 'count' => count($data), 'data' => $data, 'options'=>$options]);
     exit;
 }
 
